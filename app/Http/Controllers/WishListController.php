42135948856657ac3be6c1138f97efcd7b525a73
mdmuhir <mdmuhiruddin18@gmail.com>
2026-01-28 21:56:32 +0600

    fixed wishlist

--- app/Http/Controllers/WishListController.php
+++ app/Http/Controllers/WishListController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\WishList;
 use Illuminate\Http\Request;
 
-class WishListController extends Controller {
-    public function getWishList(Request $request) {
-        $wishlist = WishList::with('product')->where('user_id', $request->user()->id)->get();
+class WishListController extends Controller
+{
+    public function getWishList(Request $request)
+    {
+        $wishlist = WishList::with('product.images')->where('user_id', $request->user()->id)->get();
+
         return $this->success('Get wishlist', $wishlist);
     }
 
-    public function addToWishList(Request $request) {
+    public function addToWishList(Request $request)
+    {
 
-        $wishlist = new WishList();
+        $wishlist = new WishList;
         $wishlist->user_id = $request->user()->id;
         $wishlist->product_id = $request->product_id;
         $wishlist->save();
@@ -21,18 +25,17 @@ class WishListController extends Controller {
         return $this->success('Add to wishlist', $wishlist);
     }
 
-    public function deleteWishList(Request $request, $id) {
+    public function deleteWishList(Request $request, $id)
+    {
         // return $request->user()->id.">".$id;
-      
+
         $wishlist = WishList::where('user_id', $request->user()->id)->where('id', $id)->first();
-        
+
         // if(!$wishlist){
         // return $this->success('Wishlist Product not Found', null);}
 
         $wishlist->delete();
-       
+
         return $this->success('Delete wishlist', $wishlist);
     }
-
-    
 }
